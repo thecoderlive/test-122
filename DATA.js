936983f098bf1dec02e8478ec41564ca4d97439b
The Coder <82015917+thecoderlive@users.noreply.github.com>
2022-10-05 14:40:10 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "51b0a8da-9957-49f9-9628-e570ea8cb66c",
+    "home_feed": [
+        {
+            "id": "a182626d-3b93-442a-b8a3-6d758653220d",
+            "artist_name": "after",
+            "home_carousel": [
+                {
+                    "id": "a8b61f51-5074-4c86-83b9-f12ef899fae6",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/290.png"
+                },
+                {
+                    "id": "30fb393e-e308-448e-8d64-bda38a640220",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/256.png"
+                },
+                {
+                    "id": "7a1995d1-6f94-416b-b97d-64041ff1bd63",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/334.png"
+                },
+                {
+                    "id": "608e21fb-9867-423c-b2fb-58a234e1c3a6",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/271.png"
+                },
+                {
+                    "id": "57595375-ac2e-4c35-8b8a-c2adf8a10746",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/263.png"
+                }
+            ],
+            "art_caption": "herself",
+            "art_description": "Street develop remember boy share others. Well mean or visit blood seem while pretty. Eye increase population become."
+        },
+        {
+            "id": "684f4851-ea71-4661-939e-a2fa67e7ce7c",
+            "artist_name": "which",
+            "home_carousel": [
+                {
+                    "id": "0d5d85ed-2fcd-40c2-9c71-9e810e377258",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/263.png"
+                },
+                {
+                    "id": "663a8031-044a-4dd0-8c1a-3bbd6c5a145e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/132.png"
+                },
+                {
+                    "id": "736d3de9-d79b-46e9-856c-b0776879e378",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/363.png"
+                },
+                {
+                    "id": "fd1d0a85-c2bb-42c3-98bb-30246df4d74b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/257.png"
+                },
+                {
+                    "id": "4c719000-e96b-4fa6-b41a-b0c9046cd36b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/286.png"
+                }
+            ],
+            "art_caption": "majority",
+            "art_description": "Simple board both. Medical education president worker easy discuss office. Send lot brother network staff."
+        },
+        {
+            "id": "b8d85f78-034c-48d3-b593-e9e20f23c6ba",
+            "artist_name": "condition",
+            "home_carousel": [
+                {
+                    "id": "6600f89d-b7c0-4014-bc20-1384fb55f7e5",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/185.png"
+                },
+                {
+                    "id": "d27dd88d-ca10-491c-bac3-457f9a74974e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/213.png"
+                },
+                {
+                    "id": "cba920b8-9fe2-4f84-901d-f6be4b1e7d83",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/63.png"
+                },
+                {
+                    "id": "f9458039-0639-4f36-964d-11c421c3eeab",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/84.png"
+                },
+                {
+                    "id": "61204c5d-c433-4906-8552-1c60262a97e4",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/104.png"
+                }
+            ],
+            "art_caption": "group",
+            "art_description": "When weight entire human. Finish bag responsibility traditional choice. Order nor art wrong well call base. Material option garden idea full answer."
+        },
+        {
+            "id": "2cded4b8-6b5f-43f5-9b11-7e882d0d816d",
+            "artist_name": "role",
+            "home_carousel": [
+                {
+                    "id": "0f42a9b2-22d7-4438-88ab-07751490c276",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/13.png"
+                },
+                {
+                    "id": "5a078b33-187b-4c36-b86b-a2b1c32e695c",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/87.png"
+                },
+                {
+                    "id": "d13d8053-0d06-427f-9c58-3efa8961df84",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/76.png"
+                },
+                {
+                    "id": "7737653b-b977-4c8e-b8a0-41c3ac3ffabd",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/88.png"
+                },
+                {
+                    "id": "322fa254-f7e9-4e2b-bc58-e774a8bc3267",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/189.png"
+                }
+            ],
+            "art_caption": "join",
+            "art_description": "Follow term ever authority. Production before occur sing technology small give them. Past us too simple."
+        },
+        {
+            "id": "5bb599b4-c27d-4ab4-9e00-a15766766120",
+            "artist_name": "total",
+            "home_carousel": [
+                {
+                    "id": "3285b949-93fe-44eb-b7ea-5d5be8662857",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/331.png"
+                },
+                {
+                    "id": "bb9e358b-e521-4750-bba0-98820e7c1d4e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/5.png"
+                },
+                {
+                    "id": "628597f5-d282-4b49-a4f6-dc0bc0952765",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/273.png"
+                },
+                {
+                    "id": "003f38e3-186e-43a8-ad63-219898472464",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/330.png"
+                },
+                {
+                    "id": "74ac87ed-eb07-404d-8e93-f9349b8a7923",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/276.png"
+                }
+            ],
+            "art_caption": "market",
+            "art_description": "Factor right go high. Stop police charge there air though list."
+        }
+    ]
+}
